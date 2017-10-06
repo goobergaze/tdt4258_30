121287500624e32b56897eaebb61fba361a0e8ea
Krithu <kristat@stud.ntnu.no>
2017-10-06 14:13:13 +0200

    Update dac.c

--- ex2/ex2/dac.c
+++ ex2/ex2/dac.c
@@ -15,4 +15,9 @@ void setupDAC()
 	 * data registers, DAC0_CH0DATA and DAC0_CH1DATA, for example from a
 	 * timer interrupt 
 	 */
+	*CMU_HFPERCLKEN0 |= (1 << 17);
+	*DAC0_CTRL = 0x50010;
+	*DAC0_CH0CTRL = 1;
+	*DAC0_CH1CTRL = 1;
+	
 }
