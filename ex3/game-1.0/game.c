0a7c1e908f308d10bed3ff71257c6ea84f732fba
Grzegorz Swiderski <Grzegorz Swiderski>
2017-11-19 08:38:33 +0100

    Added a title screen, reorganized game code

--- ex3/game-1.0/game.c
+++ ex3/game-1.0/game.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <signal.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
-int setup_signal_handler();
+int draw_splash_screen(int);
+int setup_signal_handler(int);
 void button_handler(int);
 void iterate_game(void);
 
@@ -17,19 +17,7 @@ int fd_fb0, fd_gamepad;
 
 int main(int argc, char *argv[])
 {
-	/****************************** Framebuffer ******************************/
-
-	// Open the framebuffer device
-	fd_fb0 = open("/dev/fb0", 0);
-	if(fd_fb0 == -1)
-	{
-		puts("Failed to open framebuffer device.");
-		exit(EXIT_FAILURE);
-	}
-
-	/******************************** Gamepad ********************************/
-
-	// Open the gamepad driver with asynchronous signaling
+	// Open the gamepad driver
 	fd_gamepad = open("/dev/gamepad", O_RDONLY);
 	if(fd_gamepad == -1)
 	{
@@ -37,14 +25,29 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	// Enable listening to signals from the gamepad driver
-	if(setup_signal_handler() == -1)
+	// Open the framebuffer device
+	fd_fb0 = open("/dev/fb0", O_WRONLY);
+	if(fd_fb0 == -1)
+	{
+		puts("Failed to open framebuffer device.");
+		exit(EXIT_FAILURE);
+	}
+
+	// Draw the splash screen
+	if(draw_splash_screen(fd_fb0) == -1)
 	{
 		exit(EXIT_FAILURE);
 	}
 
-	/**************************** The Game Itself ****************************/
+	// Enable listening to signals from the gamepad driver
+	if(setup_signal_handler(fd_gamepad) == -1)
+	{
+		exit(EXIT_FAILURE);
+	}
 
+
+
+	// Main game loop
 	while(1)
 	{
 		iterate_game();
@@ -57,43 +60,6 @@ int main(int argc, char *argv[])
 	exit(EXIT_SUCCESS);
 }
 
-
-int setup_signal_handler()
-{
-	// Indicate that no signal besides SIGIO should be blocked
-	sigset_t empty_mask;
-	sigemptyset(&empty_mask);
-
-	// Define the action for receiving a signal from the gamepad driver
-	const struct sigaction act =
-	{
-		.sa_handler = button_handler,
-		.sa_mask = empty_mask
-	};
-
-	// Enable the button handler using the action object
-	if(sigaction(SIGIO, &act, NULL) == -1)
-	{
-		puts("Failed to install signal-catching function.");
-		return -1;
-	}
-
-	// Specify that this process is the owner of the gamepad driver
-	if(fcntl(fd_gamepad, F_SETOWN, getpid()) == -1)
-	{
-		puts("Failed to specify gamepad driver owner.");
-		return -1;
-	}
-
-	// Set the FASYNC flag so that this process can receive notifications
-	if(fcntl(fd_gamepad, F_SETFL, FASYNC | fcntl(fd_gamepad, F_GETFL)) == -1)
-	{
-		puts("Failed to set FASYNC flag in gamepad driver.");
-		return -1;
-	}
-
-	return 0;
-}
 
 void button_handler(int signo)
 {
---
+++ ex3/game-1.0/helpers.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define FRAMEBUFFER_SIZE 320 * 240 * 2 // Resolution * bytes per pixel
+
+void button_handler(int);
+
+// Symbols for accessing binary data included by the linker
+extern unsigned char _binary_splash_bin_start;
+extern unsigned char _binary_splash_bin_end;
+
+
+int draw_splash_screen(int fd)
+{
+	// '_binary_splash_bin_size' does not work, probably because OS prevents it
+	if(&_binary_splash_bin_end - &_binary_splash_bin_start != FRAMEBUFFER_SIZE)
+	{
+		puts("Splash graphic size does not match framebuffer size.");
+		return -1;
+	}
+
+	// Feed the image data to the framebuffer
+	if(write(fd, &_binary_splash_bin_start, FRAMEBUFFER_SIZE) == -1)
+	{
+		puts("Failed to draw splash screen.");
+		return -1;
+	}
+
+	return 0;
+}
+
+int setup_signal_handler(int fd)
+{
+	// Indicate that no signal besides SIGIO should be blocked
+	sigset_t empty_mask;
+	sigemptyset(&empty_mask);
+
+	// Define the action for receiving a signal from the gamepad driver
+	const struct sigaction act =
+	{
+		.sa_handler = button_handler,
+		.sa_mask = empty_mask
+	};
+
+	// Enable the button handler using the action object
+	if(sigaction(SIGIO, &act, NULL) == -1)
+	{
+		puts("Failed to install signal-catching function.");
+		return -1;
+	}
+
+	// Specify that this process is the owner of the gamepad driver
+	if(fcntl(fd, F_SETOWN, getpid()) == -1)
+	{
+		puts("Failed to specify gamepad driver owner.");
+		return -1;
+	}
+
+	// Set the FASYNC flag so that this process can receive notifications
+	if(fcntl(fd, F_SETFL, FASYNC | fcntl(fd, F_GETFL)) == -1)
+	{
+		puts("Failed to set FASYNC flag in gamepad driver.");
+		return -1;
+	}
+
+	return 0;
+}
